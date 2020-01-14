2f62902ffc15770636552de82e210b4601aeb237
zornfett <18092460nine@gmail.com>
2020-01-13 18:14:00 -0800

    card header button functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 $(function () {
 	// show sidebar in mobile view
-	$('#sidebarCollapse').on('click', function () {
+	$('#sidebarCollapse').on('click', function(){
 		$('#sidebar').toggleClass('active');
 	});
+	// close card butt
+	$('.close-icon').on('click',function(){
+		$(this).closest('.card').fadeOut();
+	})
+	// minimize card-body butt
+	$(document).on('click','.minimize-icon',function(){
+		$(this).closest('.card').find('.card-body').slideToggle();
+	})
+	// sidebar links spawn various cards
+	$('.spawn-card').on('click', function(){
+		console.log('booyah!');
+		$('#card' + $(this).attr('target')).show();
+	})
 });
